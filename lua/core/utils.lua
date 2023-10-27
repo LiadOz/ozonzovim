57659e6ca7b565700c9d179822130e530df19950
Liad Oz <liadozil@gmail.com>
2023-10-27 11:37:05 +0300

    Removed import print

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -4,7 +4,6 @@ function M.lazy_require(module)
   return setmetatable({}, {
     __index = function(_, key)
       return function(...)
-        print(string.format('requiring %s %s', module, key))
         return require(module)[key](...)
       end
     end
